601f0bdfa518a585a80cbed83f2c56b0bdf3bfc2
Rajat Shrestha <70558370+sayyanstha@users.noreply.github.com>
2023-11-26 15:00:58 +0545

    Update Pdf-compressor.js

--- src/components/Pdf-compressor/Pdf-compressor.js
+++ src/components/Pdf-compressor/Pdf-compressor.js
@@ -1,17 +1,24 @@
+import React from 'react';
 import './PdfCompressor.css';
 
-function PdfCompressor(){
-    let url = "Your-Link-Here";
+function PdfCompressor() {
+  let url = "Your-Link-Here";
 
-    return(
-        <div id="body">
-        <h1 id="welcome">Welcome</h1><br/>
-        <form id="form" action={url} method="post" encType="multipart/form-data">
-            <input type="file" name="File" accept=".pdf"/><br/>
-            <input type="submit" value="Convert File"/>
-        </form>
-        </div>
-    )
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Add logic to handle form submission or file upload
+  };
+
+  return (
+    <div id="body">
+      <h1 id="welcome">Welcome</h1><br/>
+      <form id="form" onSubmit={handleSubmit} encType="multipart/form-data">
+        <input type="file" name="File" accept=".pdf"/><br/>
+        <input type="submit" value="Convert File"/>
+      </form>
+    </div>
+  );
 }
 
 export default PdfCompressor;
