6358746eb0b61143c319f6b48312e854faca8749
Rajat Shrestha <70558370+sayyanstha@users.noreply.github.com>
2023-11-26 14:33:49 +0545

    Create Header.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <h1>PDF Compressor App</h1>
+    </header>
+  );
+};
+
+export default Header;
